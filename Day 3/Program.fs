d82572d7681ffcbc00e64491c451e9ed0c4aa595
Aaron Saenz <aaron@saenz.soy>
2023-12-06 19:25:55 -0600

    Finish Day 3 Part 2

--- Day 3/Program.fs
+++ Day 3/Program.fs
@@ -3,11 +3,9 @@ open System.IO
 open System.Text.RegularExpressions
 
 let inputFile = "input.txt"
-
-let isSymbol c = not <| ((c = '.') || Char.IsDigit c)
-
 let lines = File.ReadAllLines(inputFile)
 
+// Part 1
 [| 0 .. (lines.Length - 1) |]
 |> Seq.map (fun currentRow ->
     let line = lines[currentRow]
@@ -15,28 +13,73 @@ let lines = File.ReadAllLines(inputFile)
 
     matches
     |> Seq.map (fun m ->
-        let row = if currentRow = 0 then 0 else currentRow - 1
-
-        let lastRow =
-            if currentRow = lines.Length - 1 then
-                lines.Length - 1
-            else
-                currentRow + 1
+        let rowsToScan =
+            match currentRow with
+            | 0 -> [| currentRow; currentRow + 1 |]
+            | r when r + 1 = lines.Length -> [| currentRow - 1; currentRow |]
+            | _ -> [| currentRow - 1; currentRow; currentRow + 1 |]
 
         let result =
-            [| row..lastRow |]
-            |> Seq.exists (fun r ->
-                let col = if m.Index = 0 then 0 else m.Index - 1
+            rowsToScan
+            |> Seq.exists (fun row ->
+                let colsToScan =
+                    match m.Index with
+                    | 0 ->
+                        if row = currentRow then
+                            [| m.Length |]
+                        else
+                            [| 0 .. m.Length |]
+                    | c when c + m.Length = line.Length ->
+                        if row = currentRow then
+                            [| m.Index - 1 |]
+                        else
+                            [| m.Index - 1 .. line.Length - 1 |]
+                    | _ ->
+                        if row = currentRow then
+                            [| m.Index - 1; m.Index + m.Length |]
+                        else
+                            [| m.Index - 1 .. m.Index + m.Length |]
 
-                let lastCol =
-                    if m.Index + m.Length = line.Length then
-                        line.Length - 1
-                    else
-                        m.Index + m.Length
-
-                [ col..lastCol ] |> Seq.exists (fun c -> isSymbol (lines[r][c])))
+                let isSymbol c = not <| ((c = '.') || Char.IsDigit c)
+                colsToScan |> Seq.exists (fun col -> isSymbol <| lines[row][col]))
 
         if result then int m.Value else 0)
+    |> Seq.sum)
+|> Seq.sum
+|> printfn "%i"
+
+
+// Part 2
+[| 0 .. (lines.Length - 1) |]
+|> Seq.map (fun currentRow ->
+    let line = lines[currentRow]
+    let matches = Regex.Matches(line, "\\*")
+
+    matches
+    |> Seq.map (fun m ->
+        let rowsToScan =
+            match currentRow with
+            | 0 -> [| currentRow; currentRow + 1 |]
+            | r when r + 1 = lines.Length -> [| currentRow - 1; currentRow |]
+            | _ -> [| currentRow - 1; currentRow; currentRow + 1 |]
+
+        let numbers =
+            rowsToScan
+            |> Seq.fold
+                (fun state row ->
+                    let numbers =
+                        Regex.Matches(lines[row], "\\d+")
+                        |> Seq.filter (fun numberMatch ->
+                            m.Index <= numberMatch.Index + numberMatch.Length
+                            && m.Index >= numberMatch.Index - 1)
+                        |> Seq.map (fun numberMatch -> int numberMatch.Value)
+                        |> Seq.toList
+
+
+                    state @ numbers)
+                List.empty
+
+        if numbers.Length = 2 then numbers[0] * numbers[1] else 0)
     |> Seq.sum)
 |> Seq.sum
 |> printfn "%i"
